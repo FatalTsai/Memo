0c614338ef5ce00d880260fea371c8fe2e0c8e00
fatal_tsai <cheney1003@gmail.com>
2019-09-20 11:07:26 +0800

    first commit

---
+++ app/src/main/java/com/example/memo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.memo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+implements AdapterView.OnItemClickListener,AdapterView.OnItemLongClickListener {
+
+    String[] aMemo ={"1. click to edit","2. long click to clear","3. ","4. ","5. ","6. "};
+    ListView lv;
+    ArrayAdapter<String> aa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv =(ListView) findViewById(R.id.listView);
+        aa =new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, aMemo);
+
+        lv.setAdapter(aa);
+        lv.setOnItemClickListener(this);
+        lv.setOnItemLongClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> a, View v, int pos, long id) {
+        Intent it = new Intent(this,edit.class);
+        it.putExtra("No.",pos+1);
+        it.putExtra("context",aMemo[pos]);
+        startActivity(it);
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> a, View v, int pos, long id) {
+        aMemo[pos] =(pos+1)+".";
+        aa.notifyDataSetChanged();
+        return false;
+    }
+}
